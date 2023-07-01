b6a45e543ca16e2d9aeb154ff73256ee36e1b3e8
jdggLgns <josedaniel.gonzalez.garcia@alumnos.upm.es>
2023-07-01 18:25:59 +0200

    register updated

--- src/RegisterUser.py
+++ src/RegisterUser.py
@@ -1,10 +1,12 @@
 import pymysql as pymysql
-from src.Project_properties import conexiones
+from Project_properties import conexiones
 
 
 class GestionUsuarios:
     @staticmethod
-    def register(_name, _mail, _userid, _password, con):
+    def register(_name, _mail, _userid, _password):
+        con = None
+        all_success = True
         try:
             if _userid and _password:
                 con = pymysql.connect(host=conexiones["conn_interna"], user='root', password='coindetection', db='coindetection_database')
@@ -12,9 +14,10 @@ class GestionUsuarios:
                 cur.execute("INSERT INTO usuarios (name, mail, userid, password) VALUES (%s, %s, %s, %s)", (_name, _mail, _userid, _password))
                 con.commit()
             else:
-                print("error")
+                all_success = False
         except BaseException as e:
-            print("error " + e)
+                all_success = False
         finally:
             if con:
                 con.close()
+            return all_success
---
+++ src/ServicesManager.py
@@ -0,0 +1,25 @@
+#!/home/ubuntu/envyolo/bin/python3
+import json
+from flask import Flask
+from flask import request
+from RegisterUser import GestionUsuarios
+
+
+app = Flask(__name__)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+        data = request.get_json()
+        name = data.get('name')
+        mail = data.get('mail')
+        userid = data.get('userid')
+        password = data.get('password')
+
+        all_success = GestionUsuarios.register(name,mail,userid,password)
+        response = {'success': all_success}
+        return json.dumps(response)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
