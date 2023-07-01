441e9a3751ecfdd503898a2cf2a998f3d784cbd6
jdggLgns <josedaniel.gonzalez.garcia@alumnos.upm.es>
2023-07-01 15:59:51 +0200

    initial commit

---
+++ src/RegisterUser.py
@@ -0,0 +1,23 @@
+import pymysql as pymysql
+from src.Project_properties import conexiones
+
+
+class GestionUsuarios:
+    @staticmethod
+    def register(_name, _mail, _userid, _password, con):
+        try:
+            if _userid and _password:
+                con = pymysql.connect(host=conexiones["conn_interna"], user='root', password='coindetection', db='coindetection_database')
+                cur = con.cursor()
+                cur.execute("INSERT INTO usuarios (name, mail, userid, password) VALUES (%s, %s, %s, %s)", (_name, _mail, _userid, _password))
+                con.commit()
+            else:
+                print("error")
+        except BaseException as e:
+            response = {'success': False}
+        finally:
+            if con:
+                con.close()
+
+
+
